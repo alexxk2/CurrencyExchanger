9c262def57da55649201060334d8d8480b71ca5a
alexxk2 <alexx.koda@gmail.com>
2024-02-26 12:07:01 +0300

    Making single activity and project structure

---
+++ app/src/main/java/com/example/currencyexchanger/presentation/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.currencyexchanger.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.currencyexchanger.R
+import com.example.currencyexchanger.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        navController = navHostFragment.navController
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+
+    }
+}
---
+++ app/src/main/java/com/example/currencyexchanger/presentation/exchanger/view_model/ExchangerViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.currencyexchanger.presentation.exchanger.view_model
+
+import androidx.lifecycle.ViewModel
+
+class ExchangerViewModel: ViewModel() {
+}
